4ba08a7b6e1092c5b48ff1e846c1bbac8183a9d9
Taylor1818 <tbcmorgan@gmail.com>
2018-04-07 10:02:24 -0700

    Fixed camera and shuffleboard.json layout to accommodate

--- src/org/usfirst/frc1388/Robot.java
+++ src/org/usfirst/frc1388/Robot.java
@@ -103,7 +103,7 @@ public class Robot extends TimedRobot {
         gyro.calibrate();
 
 		// camera, TODO add check for Camera to see if camera is on robot, otherwise do not stop capture 
-		CameraServer.getInstance().startAutomaticCapture("Front Camera", 0);
+		CameraServer.getInstance().startAutomaticCapture();
 		
 		//elevator encoder
 		//SmartDashboard.getData(elevator.getHeight());
